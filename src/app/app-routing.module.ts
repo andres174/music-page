e56a4eb69f90655ba5536ec94f1b7e0cf8924e7a
andres174 <andresfer.zambrano@espam.edu.ec>
2022-05-28 22:36:20 -0500

    estructura inicial y home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { GenresComponent } from './pages/genres/genres.component';
+import { InfoComponent } from './pages/info/info.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent 
+  },
+  {
+    path: 'search', component: SearchComponent 
+  },
+  {
+    path: 'genres', component: GenresComponent
+  },
+  {
+    path: 'info', component: InfoComponent
+  },
+  {
+    path: 'aboutus', component: AboutUsComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './secciones/header/header.component';
+import { BodyComponent } from './secciones/body/body.component';
+import { FooterComponent } from './secciones/footer/footer.component';
+import { SearchComponent } from './pages/search/search.component';
+import { GenresComponent } from './pages/genres/genres.component';
+import { InfoComponent } from './pages/info/info.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    SearchComponent,
+    GenresComponent,
+    InfoComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
